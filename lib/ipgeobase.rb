ffaf67320cc78a826945ce8a24f26318298a87e6
Nory Baichorov <nory.baichorov@gmail.com>
2024-03-21 15:36:35 +0300

    configured gem

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
 require_relative "ipgeobase/version"
-
+require 'net/http'
+require 'happymapper'
 module Ipgeobase
   class Error < StandardError; end
-  # Your code goes here...
+  def self.lookup(ip)
+    data = get_data(ip)
+
+    parsed_data = HappyMapper.parse(data)
+
+    parsed_data
+  end
+
+  private
+  def self.get_data(ip)
+    url = URI.parse("http://ip-api.com/xml/#{ip}")
+
+    req = Net::HTTP::Get.new(URI.parse("http://ip-api.com/xml/#{ip}"))
+
+    res = Net::HTTP.start(url.host, url.port) {|http| http.request(req)}
+
+    res.body
+  end
 end
--- test/test_ipgeobase.rb
+++ test/test_ipgeobase.rb
@@ -1,13 +1,34 @@
 # frozen_string_literal: true
+require 'simplecov'
+SimpleCov.start
 
-require "test_helper"
+require_relative "test_helper"
+require 'webmock/minitest'
 
 class TestIpgeobase < Minitest::Test
+  WebMock.enable!
+
   def test_that_it_has_a_version_number
     refute_nil ::Ipgeobase::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  # def test_it_does_something_useful
+  #   assert false
+  # end
+
+  def test_stub_request
+    ip = '8.8.8.8'
+    endpoint = URI.parse("http://ip-api.com/xml/#{ip}")
+
+    request_params = {
+        'Accept' => '*/*',
+        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent' => 'Ruby'
+      }
+
+
+    stub_request(:get, endpoint).with(headers: request_params).to_return(status: 200)
+
+    Net::HTTP.get(endpoint)
   end
 end
